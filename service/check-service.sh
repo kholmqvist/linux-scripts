d5b6b4a6cc91238e052a8c707e356ba2510d3a3c
Kenneth Holmqvist <kho@nilpeter.com>
2024-11-29 13:45:56 +0100

    modified:   service/check-service.sh

--- service/check-service.sh
+++ service/check-service.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 while getopts ":n:" opt; do
-    case ${opt} in
-				n) SERVICE="${OPTARG}" ;;
-        \?) echo "Invalid option: -${OPTARG}" >&2
-            exit 1 ;;
-        :) echo "Option -${OPTARG} requires an argument." >&2
-            exit 1 ;;
-    esac
+  case ${opt} in
+	  n) SERVICE="${OPTARG}" ;;
+    \?) echo "Invalid option: -${OPTARG}" >&2
+        exit 1 ;;
+    :) echo "Option -${OPTARG} requires an argument." >&2
+        exit 1 ;;
+  esac
 done
 
 # Check the status of a specific service 
