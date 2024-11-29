b3bdbfcb409e84fd969bd302752ca45e0dbea3e4
Kenneth Holmqvist <kho@nilpeter.com>
2024-11-29 12:10:50 +0100

    modified:   backup/README.md 	modified:   backup/backup.sh

--- backup/README.md
+++ backup/README.md
@@ -1,5 +1,6 @@
 ## backup.sh
-- NB. Please remember to edit source/destination
-
 - This script compresses the specified directory into a `.tar.gz` file and saves it with a timestamp.
 - Ideal for scheduling backups via cron jobs.
+
+### Usage:
+./backup.sh -s /path/to/source -d /path/to/destination -n your-backup-name
--- backup/backup.sh
+++ backup/backup.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+while getopts ":s:d:n:" opt; do
+    case ${opt} in
+        s) SOURCE="${OPTARG}" ;;
+        d) DEST="${OPTARG}" ;;
+				n) NAME="${OPTARG}" ;;
+        \?) echo "Invalid option: -${OPTARG}" >&2
+            exit 1 ;;
+        :) echo "Option -${OPTARG} requires an argument." >&2
+            exit 1 ;;
+    esac
+done
+
 # Backup a directory and store it in a backup folder with a timestamp
-SOURCE="/path/to/important/data"
-DEST="/path/to/backup/location"
 TIMESTAMP=$(date +"%Y%m%d%H%M%S")
-tar -czvf $DEST/backup_$TIMESTAMP.tar.gz $SOURCE
-echo "Backup completed: $DEST/backup_$TIMESTAMP.tar.gz"
+tar -czvf ${DEST}/${NAME}_backup_${TIMESTAMP}.tar.gz ${SOURCE}
+echo "Backup completed: ${DEST}/${NAME}_backup_${TIMESTAMP}.tar.gz"
