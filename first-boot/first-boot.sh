d8afeebb72f94a2e19b08b76292f1c6687ab26a2
Kenneth Holmqvist <kho@nilpeter.com>
2024-11-29 14:15:31 +0100

    modified:   first-boot.sh 	new file:   remove-first-boot.sh

--- first-boot/first-boot.sh
+++ first-boot/first-boot.sh
@@ -12,5 +12,13 @@ echo -n "New Hostname: "
 read NAME
 bash ${HOME}/NP-Linux-Scripts/hostname/set-hostname.sh -n ${NAME}
 
+
+#Reset SSH-Keys
+sudo rm /etc/ssh/ssh_host_*_key* 2>&1
+ssh-keygen -q -N "" -t rsa -b 4096 -f /etc/ssh/ssh_host_rsa_key
+ssh-keygen -q -N "" -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key
+ssh-keygen -q -N "" -t ed25519 -f /etc/ssh/ssh_host_ed25519_key
+sudo systemctl restart ssh
+
 # Run this command as the last command before exiting
 touch $HOME/.first-boot > /dev/null 2>&1
