eb045cef46cbae88e19b491d75c5d904b6d97071
Kenneth Holmqvist <kho@nilpeter.com>
2024-11-29 13:12:13 +0100

    modified:   ssl/check-certificate.sh

--- ssl/check-certificate.sh
+++ ssl/check-certificate.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-while getopts ":d" opt; do
+while getopts ":d:p" opt; do
     case ${opt} in
         d) DOMAIN="${OPTARG}" ;;
+				p) PORT="${OPTARG}" ;;
         \?) echo "Invalid option: -${OPTARG}" >&2
             exit 1 ;;
         :) echo "Option -${OPTARG} requires an argument." >&2
@@ -11,6 +12,6 @@ while getopts ":d" opt; do
 done
 
 # Check SSL certificate expiration 
-EXPIRY_DATE=$(echo | openssl s_client -servername ${DOMAIN} -connect ${DOMAIN}:443 2>/dev/null | openssl x509 -noout -dates | grep notAfter | cut -d= -f2)
+EXPIRY_DATE=$(echo | openssl s_client -servername ${DOMAIN} -connect ${DOMAIN}:${PORT:-443} 2>/dev/null | openssl x509 -noout -dates | grep notAfter | cut -d= -f2)
 DAYS_LEFT=$(( ($(date -d "${EXPIRY_DATE}" +%s) - $(date +%s)) / 86400 ))
 echo "SSL certificate for ${DOMAIN} expires in ${DAYS_LEFT} days."
